14521bae66a931e057723bcce738e955efd44d70
daniel olivier <morganz55057@gmail.com>
2019-12-11 17:16:01 +0100

    testing

--- src/files.h
+++ src/files.h
@@ -15,8 +15,7 @@ void save_ranking();
 
 int findSize(char file_name[]);
 
-void read_files(int qualified_cars[], int race_ranking[], int last_cars_of_Q1[], int last_cars_of_Q2[], char file[],
-                int lines_to_read);
+void read_files(int qualified_cars[], int race_ranking[], int last_cars_of_Q1[], int last_cars_of_Q2[], char file[], int lines_to_read);
 
 void save_eliminated_cars(char file_to_save[], int array[]);
 
